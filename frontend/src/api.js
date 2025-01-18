ee6ee9b8ebb7b6565c41b17ac204a0a650b94923
Isabela Nery <isabelanery.r@gmail.com>
2025-01-18 16:43:58 -0300

    feat: video folders list and filter by folder

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -34,6 +34,38 @@ export async function fecthVideosListData(apiKey) {
   }
 }
 
+export async function fetchFoldesData(apiKey) {
+  try {
+    const url = "https://api-v2.pandavideo.com.br/folders";
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Authorization": `${apiKey}`,
+        "Accept": "application/json"
+      }
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Dados recebidos:", data);
+
+      return data;
+    }
+  } catch (error) {
+    console.error("Erro ao realizar a requisição:", error);
+    return;
+  }
+}
+
+export async function getFolders(apiKey) {
+  const data = await fetchFoldesData(apiKey);
+
+  return data?.folders.map((folder) => (
+    { name: folder.name, id: folder.id, videoCount: folder.videos_count }
+  ));
+}
+
 const API_KEY = 'apiKey';
 
 export function getApiKey() {
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -4,20 +4,31 @@
   <v-container width="800px" class="mt-6">
     <SearchBar v-model="searchQuery" />
 
-    <div width="800px" class="mt-16 centered-loading" v-if="isLoading">
-      <v-progress-circular
-        indeterminate
-        size="50"
-        color="primary"
-        class="d-flex justify-center mt-4"
-      ></v-progress-circular>
+    <div v-if="isLoading" width="800px" class="mt-16 centered-loading">
+      <v-progress-circular indeterminate size="50" color="primary" class="d-flex justify-center mt-4"></v-progress-circular>
     </div>
 
-    <v-row v-else>
-      <v-col v-for="video in filteredVideos" :key="video.id" cols="12" md="4">
-        <VideoCard :video="video" @click="openVideo(video.id)" />
-      </v-col>
-    </v-row>
+    <div v-else>
+      <div class="mt-4">
+        <v-chip
+          v-for="folder in folders"
+          :key="folder.id"
+          :color="selectedFolder === folder.id ? 'primary' : ''"
+          :variant="selectedFolder === folder.id ? 'flat' : 'tonal'"
+          @click="filterByFolder(folder.id)"
+          class="mr-2"
+        >
+          {{ folder.name }} ({{ folder.videoCount }})
+        </v-chip>
+      </div>
+
+      <v-row class="mt-8">
+        <v-col v-for="video in filteredVideos" :key="video.id" cols="12" md="4">
+          <VideoCard :video="video" @click="openVideo(video.id)" />
+        </v-col>
+      </v-row>
+    </div>
+
 
     <EmptyState v-if="!isLoading && filteredVideos.length === 0" message="Nenhum vídeo encontrado." />
   </v-container>
@@ -27,7 +38,7 @@
 import SearchBar from '../components/SearchBar.vue';
 import VideoCard from '../components/VideoCard.vue';
 import EmptyState from '../components/EmptyState.vue';
-import { deleteApiKey, fecthVideosListData, getApiKey } from '../api';
+import { deleteApiKey, fecthVideosListData, getApiKey, getFolders } from '../api';
 
 export default {
   components: { SearchBar, VideoCard, EmptyState },
@@ -35,30 +46,39 @@ export default {
     return {
       apiKey: '',
       videos: [],
+      folders: [],
+      selectedFolder: null,
       searchQuery: '',
       isLoading: false,
     };
   },
   computed: {
     filteredVideos() {
-      return this.videos.filter(video =>
-        video.title.toLowerCase().includes(this.searchQuery.toLowerCase())
-      );
+      return this.videos.filter(video => {
+        const matchesSearch = video.title.toLowerCase().includes(this.searchQuery.toLowerCase());
+        const matchesFolder = this.selectedFolder ? video.folder_id === this.selectedFolder : true;
+
+        return matchesSearch && matchesFolder;
+      });
     },
   },
   methods: {
-    async fetchVideos() {
+    async fetchData() {
       this.isLoading = true;
 
       try {
         const data = await fecthVideosListData(this.apiKey);
         this.videos = data.videos || [];
+        this.folders = await getFolders(this.apiKey) || [];
+
         this.isLoading = false;
       } catch (error) {
-        console.error("Erro ao buscar vídeos:", error);
+        console.error("Erro ao buscar dados da API:", error);
         this.$router.push('/');
-        return;
       }
+    },
+    filterByFolder(folderId) {
+      this.selectedFolder = this.selectedFolder === folderId ? null : folderId;
     },
     openVideo(id) {
       this.$router.push(`/video/${id}`);
@@ -75,17 +95,19 @@ export default {
       this.$router.push('/');
     }
   },
-  mounted() {
+  async mounted() {
     this.apiKey = getApiKey() || '';
 
-    if (this.apiKey) this.fetchVideos();
+    if (this.apiKey) {
+      await this.fetchData();
+    }
   },
 };
 </script>
 
 <style>
-  .centered-loading {
-    display: flex;
-    justify-content: center;
-  }
+.centered-loading {
+  display: flex;
+  justify-content: center;
+}
 </style>
