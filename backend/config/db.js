35344870cdb9cbf060f780de4d3ee34667780c70
Isabela Nery <isabelanery.r@gmail.com>
2025-01-19 17:23:47 -0300

    config: sequelize

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+require('dotenv').config(); // Carrega as variáveis do arquivo .env
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,  // Usando 5432 como porta padrão se não definida
+});
+
+module.exports = sequelize;
