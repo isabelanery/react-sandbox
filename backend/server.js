703d9c6ada9252a1e7915da1bb80a34f9eae79bb
Isabela Nery <isabelanery.r@gmail.com>
2025-01-20 10:23:48 -0300

    refactory: server - create user routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('./models/user');
 const sequelize = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 app.use(express.json());
@@ -14,32 +12,7 @@ sequelize.sync().then(() => {
   console.log('Banco de dados conectado');
 });
 
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return res.status(400).json({ message: 'Email ou senha incorretos' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Email ou senha incorretos' });
-    }
-
-    const token = jwt.sign(
-      { userId: user.id },
-      process.env.JWT_SECRET,
-      { expiresIn: '7d' }
-    );
-
-    res.json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Erro interno do servidor' });
-  }
-});
+app.use('/api', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API funcionando!');
